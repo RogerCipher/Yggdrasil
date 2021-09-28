bbdfbd83e131d37afa07739a120f1dd815ba6a56
RogerCipher <61879622+RogerCipher@users.noreply.github.com>
2021-09-28 03:22:15 +0100

    minmax pt 1
    implemented a quick min max resolver, needs to be revised to make it recursive till the root

--- tree.c
+++ tree.c
@@ -35,14 +35,14 @@ int adicionarFilho(TipoFolha *parent, TipoFolha *child)
     {
         //nao é o primeiro filho, vamos iterar ate ao ultimo filho
         TipoFolha *iterador = parent->children;
-        while (iterador->siblings != NULL) 
+        while (iterador->nextSibling != NULL) 
         {
             //iterar filho a filho ate ao ultimo
-            iterador = iterador->siblings;
+            iterador = iterador->nextSibling;
         }
 
         //adicionar este filho como irmao do ultimo irmao actual
-        iterador->siblings = child;
+        iterador->nextSibling = child;
     }
     return 1;
 }
@@ -52,12 +52,12 @@ TipoFolha *novaFolha(TipoFolha *parent)
     //criar uma nova folha
     TipoFolha *novoElem = (TipoFolha *)malloc(sizeof(TipoFolha));
     if (novoElem == NULL) {
-        printf("Cannot allocate memory for new tree node\n");
+        printf("Nao foi possivel alocar memoria para uma das folhas da arvore\n");
         return NULL;
     }
 
     //popular a folha com NULL
-    novoElem->siblings = NULL;
+    novoElem->nextSibling = NULL;
     novoElem->children = NULL;
     novoElem->data = NULL;
 
@@ -88,7 +88,7 @@ void imprimirGraphviz(TipoFolha *elem)
     if (elem->data == NULL) 
     {
         //elemento nao tem valor
-        printf("\tn%p [label = \"\"]\n", elem);
+        printf("\tn%p [label = \"N/A\"]\n", elem);
     } 
     else 
     {
@@ -107,8 +107,8 @@ void imprimirGraphviz(TipoFolha *elem)
 
         //iterar e ir imprimindo cada filho subsequente
         TipoFolha *iterador = elem->children;
-        while (iterador->siblings != NULL) {
-            iterador = iterador->siblings;
+        while (iterador->nextSibling != NULL) {
+            iterador = iterador->nextSibling;
             imprimirGraphviz(iterador);
         }
     }
@@ -125,9 +125,9 @@ void freeArvore(TipoFolha *elem)
         return;
     }
     TipoFolha *iterador = elem->children;
-    while ((iterador != NULL) && (iterador->siblings != NULL)) {
+    while ((iterador != NULL) && (iterador->nextSibling != NULL)) {
         freeArvore(iterador);
-        iterador = iterador->siblings;
+        iterador = iterador->nextSibling;
     }
     elem->children = NULL;
     if (elem->data != NULL) {
@@ -218,4 +218,98 @@ TipoFolha *carregarArvoreDeUmFicheiro(char *nomeFicheiro)
         return NULL;
     }
     return raiz;
+}
+
+TipoData *minmax(TipoFolha *elemento)
+{
+
+    if(elemento->children == NULL)
+    {
+        //se nao ha mais filhos vamos dar return do valor da folha actual
+        return elemento->data;
+    }
+
+    TipoFolha *iterador = elemento->children;
+    TipoData *dataActual;
+
+    //ja passou um numero para cima, nos queremos manter o 
+    //maior numero se for max ou menor se for min, aka
+    //max se for par ou 0, min se for impar
+    if(iterador->parent->nivelDaFolha % 2 == 0 || iterador->parent->nivelDaFolha == 0)
+    {
+        printf("avaluating max player\n");
+        //max player
+        while(iterador != NULL)
+        {
+            printf("\nmax\n");
+            
+
+            dataActual = minmax(iterador);
+            
+            printf("\nmax found: %d\n", dataActual->value);
+            if(iterador->data == NULL)
+            {
+                iterador->data = (TipoData *)malloc(sizeof(TipoData));
+                iterador->data->value = dataActual->value;
+            }
+            else if(iterador->data->value < dataActual->value)
+            {
+                iterador->data->value = dataActual->value;
+            }
+            printf("\ncurrent max value: %d\n", iterador->data->value);
+            iterador = iterador->nextSibling;
+        } 
+    }
+    else
+    {
+        printf("avaluating min player\n");
+        //min player
+        while(iterador != NULL)
+        {
+            printf("\nmin\n");
+
+
+            dataActual = minmax(iterador);
+
+
+            printf("\nmin found: %d\n", dataActual->value);
+            if(iterador->data == NULL)
+            {
+                iterador->data = (TipoData *)malloc(sizeof(TipoData));
+                iterador->data->value = dataActual->value;
+            }
+            else if(iterador->data->value > dataActual->value)
+            {
+                iterador->data->value = dataActual->value;
+            }
+            printf("\ncurrent min value: %d\n", iterador->data->value);
+            iterador = iterador->nextSibling;
+        } 
+    }
+
+    if(elemento->parent == NULL)
+    {
+        //se chegarmos a raiz basta fazermos o max do resultado final:
+        
+        iterador = elemento->children;
+        while(iterador != NULL)
+        {
+            if(elemento->data == NULL)
+            {
+                elemento->data = (TipoData *)malloc(sizeof(TipoData));
+                elemento->data->value = elemento->children->data->value;
+            }
+            else
+            {
+                if(iterador->data->value > elemento->data->value)
+                {
+                    elemento->data->value = iterador->data->value;
+                }
+            }
+            iterador = iterador->nextSibling;
+        }
+        return elemento->data;
+    }
+
+    return dataActual;
 }
