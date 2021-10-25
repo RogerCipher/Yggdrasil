2ad5bab73020afd8a369889fbcff6ba1caca67f4
RogerCipher <61879622+RogerCipher@users.noreply.github.com>
2021-10-25 04:23:17 +0100

    implementing huffman pt.1

--- huffman.c
+++ huffman.c
@@ -16,132 +16,7 @@ by: Rogério Chaves (AKA CandyCrayon), 2021
 
 #include "huffman.h"
 
-TipoNode *createWeightedList(char *nomeFicheiro)
-{
-    FILE *ficheiro = fopen(nomeFicheiro, "r");
-    if(ficheiro == NULL)
-    {
-        printf("Couldnt find file\n");
-        exit(1);
-    }
-
-    char currentChar = fgetc(ficheiro);
-    if(currentChar == EOF)
-    {
-        printf("File was empty\n");
-        exit(1);
-    }
-
-    TipoNode *cabeca =  (TipoNode *)malloc(sizeof(TipoNode));
-    cabeca->left = NULL;
-    cabeca->parent = NULL;
-    cabeca->right = NULL;
-    cabeca->value = currentChar;
-    cabeca->weight = 1;
-
-    TipoNode *iterador;
-    int found;
-    currentChar = (char)fgetc(ficheiro);
-    while(currentChar != EOF)
-    {
-        //para cada char no ficheiro:
-        found = 0;
-        iterador = cabeca;
-        while (iterador != NULL)
-        {
-            //iterar pela linked list ate encontrarmos um elemento
-            //com este "value"
-            if(iterador->value == currentChar)
-            {
-                iterador->weight++;
-                found = 1;
-                break;
-            }
-            iterador = iterador->right;
-        }
-
-        if(!found)
-        {
-            //se nao foi encontrado vamos andar pela lista ate ao final
-            //e adicionar no final da lista um novo node com este value
-            TipoNode *novoNode = (TipoNode *)malloc(sizeof(TipoNode));
-            novoNode->parent = NULL;
-            novoNode->right = NULL;
-            novoNode->value = currentChar;
-            novoNode->weight = 1;
-
-            iterador = cabeca;
-            while (iterador->right != NULL)
-            {
-                iterador = iterador->right;
-            }
-
-            iterador->right = novoNode;
-            novoNode->left = iterador;
-        }
-        
-
-        currentChar = (char)fgetc(ficheiro);
-    }
-    printf("EOF\n");
-
-    return cabeca;
-
-
-    fclose(ficheiro);
-}
-
-int orderByLowestWeight(TipoNode *cabeca)
-{
-    //ter o tamanho da lista
-    int tamanhoLista = 0;
-    TipoNode *iterador = cabeca;
-    while(iterador != NULL)
-    {
-        tamanhoLista++;
-        iterador = iterador->right;
-    }
-
-    //temporary variables for the swap in the bubble sort
-    char tempChar;
-    int tempInt;
-
-    printf("\nbubble\n");
-    printf("tamanhoLista = %d\n", tamanhoLista);
-    //bubble sort na lista
-    for(int i = 0; i < tamanhoLista; i++)
-    {
-        printf("*i = %d\n", i);
-        //reset the iterator to the right position
-        iterador = cabeca;
-        for(int pos = 0; pos < i; pos++)
-            iterador = iterador->right;
-            
-
-        
-        for(int j = 0; j < tamanhoLista - i -1; j++)
-        {
-            printf("j = %d\n", j);
-            if(iterador->weight > iterador->right->weight)
-            {
-                //swap the information of these nodes
-                tempInt = iterador->weight;
-                tempChar = iterador->value;
-
-                iterador->weight = iterador->right->weight;
-                iterador->value = iterador->right->value;
-
-                iterador->right->weight = tempInt;
-                iterador->right->value = tempChar;
-            }
-            iterador = iterador->right;
-        }
-    }
-
-
-
-    return 0;
-}
+//TODO: INTEGRAR ISTO COM O TREE HANDLER DO TREE.C
 
 int printLL(TipoNode *cabeca)
 {
@@ -158,13 +33,184 @@ int printLL(TipoNode *cabeca)
 
 
 
+//inserir um elemento da linked list no sitio certo dependendo do weight
+int insertInWeightedList(TipoNode **cabeca, TipoNode *elemento)
+{
+    if (*cabeca == NULL) {    // a linked list esta vazia
+        *cabeca = elemento;
+        return 1;
+    }
+
+    TipoNode *iterador = *cabeca;
+    TipoNode *cauda = *cabeca;
+
+
+    while(iterador != NULL) {
+        cauda = iterador;
+        if (iterador->weight > elemento->weight) { // Insert before
+            elemento->right = iterador;
+            elemento->left = iterador->left;
+            iterador->left = elemento;
+            if (elemento->left == NULL) {
+                *cabeca = elemento;
+            } else {
+                elemento->left->right = elemento;
+            }
+            return 1;
+        }
+        iterador = iterador->right;
+    }
+
+    // Add to list
+    cauda->right = elemento;
+    elemento->left = cauda;
+
+    return 0;
+}
+
+int createWeightedList(char *nomeFicheiro, TipoNode **cabeca)
+{
+    FILE *ficheiro = fopen(nomeFicheiro, "r");
+    if(ficheiro == NULL)
+    {
+        printf("Couldnt find file\n");
+        exit(1);
+    }
+
+    char currentChar = fgetc(ficheiro);
+    if(currentChar == EOF)
+    {
+        printf("File was empty\n");
+        exit(1);
+    }
+
+    //temp array to store weight of char
+    int weights[256];
+
+    for(int i = 0; i < 256; i++)
+        //inicialize all weights at 0
+        weights[i] = 0;
+
+
+    while (currentChar != EOF)
+    {
+        //o peso do char sera guardado na posicao (int)char
+        weights[(int)currentChar]++;
+        currentChar = fgetc(ficheiro);
+    }
+    
+    fclose(ficheiro);
+
+    for(int i = 0; i < 256; i++)
+    {
+        if(weights[i] != 0)
+        {
+            TipoNode *elemento = (TipoNode *)malloc(sizeof(TipoNode));
+            elemento->value = (char)i;
+            elemento->weight = weights[i];
+            elemento->left = NULL;
+            elemento->right = NULL;
+            elemento->parent = NULL;
+            insertInWeightedList(cabeca, elemento);
+        }
+    }
+
+
+
+    printf("EOF\n");
+
+    
+
+    return 1;
+}
+
+//recebe uma linked list e retorna uma arvore
+TipoNode *huffmanTree(TipoNode **cabeca) {
+    TipoNode *left, *right;
+
+    while (1) {
+        left = (*cabeca);
+        right = (*cabeca)->right;
+
+        if (right == NULL) {    // se ja so ha 1 elemento esta feito
+            return left;
+        }
+
+        //criar o node pai
+        TipoNode *parent = (TipoNode *)malloc(sizeof(TipoNode));
+
+        parent->value = 0;
+        parent->left = left;
+        parent->right = right;
+        parent->weight = left->weight + right->weight;
+
+        //refazer a linked list actualizada
+        *cabeca = (*cabeca)->right->right;
+        (*cabeca)->left = NULL;
+        right->right = NULL;
+        
+        printLL(*cabeca);
+
+        /*
+
+        printf("left: %d : %d\n", left->value, left->weight);
+        printf("right: %d: %d\n", right->value, right->weight);
+        printf("---\n");
+
+        */
+        insertInWeightedList(cabeca, parent);
+    }
+}
+
+
+
+void printHuffTree(TipoNode *elemento)
+{
+    if (elemento->parent == NULL) 
+    {
+        //estamos na raiz
+        printf("Copy the following code to https://dreampuf.github.io/GraphvizOnline\n");
+        printf("graph {\n");
+    }
+    if (elemento->value == 0) 
+    {
+        //elemento nao tem valor
+        printf("\tn%p [label = \"N/A\"]\n", elemento);
+    } 
+    else 
+    {
+        //elemento tem valor
+        printf("\tn%p [label = \"%c : %d\"]\n", elemento, elemento->value, elemento->weight);
+    }
+        
+    if (elemento->parent != NULL) 
+    {
+        //criar ligacao do pai ao filho
+        printf("\tn%p -- n%p\n", elemento->parent, elemento);
+    }
+    if (elemento->left != NULL) 
+    {
+        //imprimir recursivamente para cada filho
+        printHuffTree(elemento->left);    // imprimir primeiro filho
+
+        if(elemento->right != NULL)
+            printHuffTree(elemento->right);
+    }
+    if (elemento->parent == NULL) {
+        //fim
+        printf("}\n");
+    }
+}
+
+
 int main(int argc, char *argv[])
 {
-    TipoNode *cabeca = createWeightedList(argv[1]);
+    TipoNode *cabeca = NULL;
+    createWeightedList(argv[1], &cabeca);
+    TipoNode *raiz = huffmanTree(&cabeca);
     printLL(cabeca);
     printf("---------\n");
-    orderByLowestWeight(cabeca);
-    printLL(cabeca);
+    //printHuffTree(raiz);
 
     printf("\nx\n");
     return 0;
